bc902c8e40b340cbd8e3f7dbcc0cd7c23e450cb7
xnohat <xnohat@SURFACEPRO8>
2022-11-10 05:06:14 +0700

    crack budibase pro license

--- packages/server/src/api/index.js
+++ packages/server/src/api/index.js
@@ -19,6 +19,31 @@ const router = new Router()
 router.get("/health", ctx => (ctx.status = 200))
 router.get("/version", ctx => (ctx.body = pkg.version))
 
+router.post('/api/users/:userId/activity', ctx => {
+
+  var resp = {"size":0,"timeout":0}
+
+  ctx.body = JSON.stringify(resp)
+  ctx.status = 201
+
+});
+
+router.post('/api/license', ctx => {
+
+  var full_license = {"features":["userGroups","appBackups"],"quotas":{"usage":{"monthly":{"queries":{"name":"Queries","value":-1,"triggers":[]},"automations":{"name":"Automations","value":-1,"triggers":[80,90,100]},"dayPasses":{"name":"Day Passes","value":-1,"triggers":[80,90,100]}},"static":{"apps":{"name":"Apps","value":-1,"triggers":[100]},"rows":{"name":"Rows","value":-1,"triggers":[90,100]},"userGroups":{"name":"User Groups","value":-1,"triggers":[80,100]},"plugins":{"name":"Plugins","value":-1,"triggers":[90,100]}}},"constant":{"automationLogRetentionDays":{"name":"Automation Logs","value":-1,"triggers":[]},"appBackupRetentionDays":{"name":"App Backups","value":-1,"triggers":[]}}},"plan":{"type":"enterprise"},"refreshedAt":"2022-11-09T12:52:00.030Z","version":"c6dcd6eff20b496e8a1c8c03aae0e9aa"}
+
+  ctx.body = JSON.stringify(full_license)
+
+});
+
+router.post('/api/license/activate', ctx => {
+
+  var full_license = {"features":["userGroups","appBackups"],"quotas":{"usage":{"monthly":{"queries":{"name":"Queries","value":-1,"triggers":[]},"automations":{"name":"Automations","value":-1,"triggers":[80,90,100]},"dayPasses":{"name":"Day Passes","value":-1,"triggers":[80,90,100]}},"static":{"apps":{"name":"Apps","value":-1,"triggers":[100]},"rows":{"name":"Rows","value":-1,"triggers":[90,100]},"userGroups":{"name":"User Groups","value":-1,"triggers":[80,100]},"plugins":{"name":"Plugins","value":-1,"triggers":[90,100]}}},"constant":{"automationLogRetentionDays":{"name":"Automation Logs","value":-1,"triggers":[]},"appBackupRetentionDays":{"name":"App Backups","value":-1,"triggers":[]}}},"plan":{"type":"enterprise"},"refreshedAt":"2022-11-09T12:52:00.030Z","version":"c6dcd6eff20b496e8a1c8c03aae0e9aa"}
+
+  ctx.body = JSON.stringify(full_license)
+
+});
+
 router
   .use(
     compress({
