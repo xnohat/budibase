ba0d4cdd835fe82b2b50964297b4ed7ee4960e9c
xnohat <xnohat@SURFACEPRO8>
2022-11-16 00:24:20 +0700

    Fixed viewport disable zoom

--- packages/server/src/api/controllers/static/templates/BudibaseApp.svelte
+++ packages/server/src/api/controllers/static/templates/BudibaseApp.svelte
@@ -12,7 +12,7 @@
   <meta charset="utf8" />
   <meta
     name="viewport"
-    content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+    content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"
   />
   <!-- Opengraph Meta Tags -->
   <meta name="twitter:card" content="summary_large_image" />
